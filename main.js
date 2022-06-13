db8bed752e73b9d011c3032f8fcb5893858f73f2
matheusmartins05 <107444082+matheusmartins05@users.noreply.github.com>
2022-06-13 20:31:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+function onScroll(){
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+  activeMenuAtCurrentSection();
+}
+
+function activeMenuAtCurrentSection(){
+  const targetLine = scrollY + innerHeight / 2;
+
+  console.log(targetLine);
+
+
+}
+
+function showNavOnScroll(){
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  }
+  else{
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll(){
+  if (scrollY > 600) {
+    backToTopButton.classList.add('show')
+  }
+  else{
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu(){
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu(){
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+    #home
+    #home img,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card,
+    #sobre,
+    #sobre header,
+    #sobre .content`);
+
+
+
